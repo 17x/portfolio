ed9b67baf365566871817533c1af11955c9459d7
yahone <yahone.chow@gmail.com>
2025-01-08 18:53:20 +0900

    #save-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,16 +11,14 @@ export default function RootLayout({
     <html lang="en">
     <head>
       <meta charSet="UTF-8" />
-      {/*      <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />*/}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-      <title>Document</title>
+      <title>Online CV</title>
     </head>
     <body>
     <Header />
     <div id="main">
-        {children}
+      {children}
     </div>
     </body>
     </html>
